bae845d6483e5252988bdf532fb4dd9d847cbb52
oleg <ksr10@mail.ru>
2015-12-25 15:55:40 +0200

    dom parser lib

--- vendor/bw/Bw/Api/Service/Connect.php
+++ vendor/bw/Bw/Api/Service/Connect.php
@@ -14,15 +14,14 @@ class Connect
         $curl->setOpt(CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1');
                 
         $curl->get($uri);
-        
+                
         if (!$curl->error) {
-            $response = $curl->response;
-            //file_put_contents($GLOBALS['root_dir'].'/data/logs/logger.log', print_r($curl->response, 1 ) . "\n", FILE_APPEND);
+            $response = $curl->response;            
         } else {
             throw new \Exception('Connection Error to ' . $uri);
         }
         
         $curl->close();
-        echo '<pre>' . var_dump('test') . '</pre>'; die();
+        return $response;
     }
 }
--- vendor/bw/Bw/Api/Service/Marathonbet.php
+++ vendor/bw/Bw/Api/Service/Marathonbet.php
@@ -2,6 +2,7 @@
 
 namespace Bw\Api\Service;
 
+use Sunra\PhpSimple\HtmlDomParser;
 
 class Marathonbet extends Connect
 {
@@ -16,8 +17,11 @@ class Marathonbet extends Connect
     {
         foreach ($this->uries as $key => $uri) {
             $params = $uri['params'];
-            $response = $this->connect($uri['uri'], false, $params);
-            
+            /*$response = $this->connect($uri['uri'], false, $params);
+            file_put_contents($GLOBALS['root_dir'].'/data/logs/example.html', $response);
+            $dom = HtmlDomParser::str_get_html($response);*/
+            $dom = HtmlDomParser::file_get_html($GLOBALS['root_dir'].'/data/logs/example.html');
+            echo '<pre>' . var_dump(get_class($dom)) . '</pre>'; die();
         }        
     }
 }
